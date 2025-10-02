df9892d678f5b6d1a4663dc8ba9928ec568e6fb7
al437588 <al437588@uji.es>
2025-10-02 18:52:14 +0200

    1a version

--- Practica1.java
+++ Practica1.java
@@ -1,5 +1,4 @@
 package practica1;
-
 import java.util.*;
 
 public class Practica1 {
@@ -7,11 +6,53 @@ public class Practica1 {
     //EJERCICIO 1
     public static Set<Integer> multiplos (Iterator<Integer> it) {
         //TODO
-        return null;
+
+        Set<Integer> sol = new HashSet<>();
+        List<Integer> lista = new ArrayList<>();
+
+        while (it.hasNext()) {
+            int anyadir = it.next();
+            if (anyadir != 0) {
+                lista.add(anyadir);
+            }
+        }
+
+        for(int i = 0; i < lista.size(); i++) {
+            int actual = lista.get(i);
+            boolean multiplo = false;
+
+            for (int j = 0; j < lista.size(); j++) {
+                int comprobar = lista.get(j);
+                if (i != j) {
+                    if ((actual % comprobar) == 0) {
+                        multiplo = true;
+                        break;
+                    }
+                }
+            }
+
+            if (multiplo) {
+                sol.add(actual);
+            }
+
+        }
+        return sol;
     }
 
     //EJERCICIO2
     public static void separate (Set<Integer> cuadrados, Set<Integer> noCuadrados)  {
+
+        Set<Integer> elementos = new HashSet<>();
+        elementos.addAll(cuadrados);
+        elementos.addAll(noCuadrados);
+
+        Set<Integer> cuadrado_elementos = new HashSet<>();
+        for(int elem : elementos){
+            cuadrado_elementos.add(elem * elem);
+        }
+
+
+
         //TODO
 
     }
